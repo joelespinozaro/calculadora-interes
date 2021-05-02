ca6c7e1b5446b34806672edcf0c50c413464489f
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-05-01 21:07:40 -0500

    Eliminando clases innecesarias

--- src/main/java/com/joelespinozaro/calculadora/CalculadoraInteresApplication.java
+++ src/main/java/com/joelespinozaro/calculadora/CalculadoraInteresApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlatziMarketApplication {
+public class CalculadoraInteresApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlatziMarketApplication.class, args);
+		SpringApplication.run(CalculadoraInteresApplication.class, args);
 	}
 
 }
